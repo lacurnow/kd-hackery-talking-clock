67e3786c797d14c582e99562f48efd41cb6e98d2
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-09-29 15:42:11 +0100

    structure refactoring

--- src/main/scala/jobs/TalkingClockJobV2.scala
+++ src/main/scala/jobs/TalkingClockJobV2.scala
@@ -1,21 +1,10 @@
-package actions
+package jobs
 
-import actions.TalkingClockActionsV2.{convertNumberToWords, getTime, timeStringBuilder}
+import actions.TalkingClockActionsV2.{getTime, timeToString}
+import actions.TextToSpeechActions.textToSpeech
 
 object TalkingClockJobV2 extends App {
   val currentTime = getTime
-
-  def timeToSpeech(currentTime: HourAndMinute) = {
-    val hourNum = currentTime.hour
-    val minNum = currentTime.minute
-    val hourWord = convertNumberToWords(if (hourNum <= 12) hourNum else hourNum % 12)
-    val minWord = convertNumberToWords(minNum)
-    val unitsMinWord = convertNumberToWords(minNum % 10)
-    val tensMinWord = convertNumberToWords(minNum - (minNum % 10))
-
-    val timeString = timeStringBuilder(hourNum, minNum, tensMinWord, unitsMinWord, minWord, hourWord)
-    timeString
-  }
-    println(timeToSpeech(currentTime))
-    TextToSpeechActions.textToSpeech(timeToSpeech(currentTime))
+  println(timeToString(currentTime))
+  textToSpeech(timeToString(currentTime))
 }
